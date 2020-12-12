5366dfde11454b5a6e6f3028061b38c2867c89aa
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2020-12-12 09:19:10 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react-wan/core'
 import 'inter-ui'
-import React, { StrictMode } from 'react'
+import React from 'react'
 import { isMobile } from 'react-device-detect'
 import ReactDOM from 'react-dom'
 import ReactGA from 'react-ga'
@@ -54,7 +54,6 @@ function Updaters() {
 }
 
 ReactDOM.render(
-  <StrictMode>
     <FixedGlobalStyle />
     <Web3ReactProvider getLibrary={getLibrary}>
       <Web3ProviderNetwork getLibrary={getLibrary}>
@@ -68,7 +67,6 @@ ReactDOM.render(
           </ThemeProvider>
         </Provider>
       </Web3ProviderNetwork>
-    </Web3ReactProvider>
-  </StrictMode>,
+    </Web3ReactProvider>,
   document.getElementById('root')
 )
