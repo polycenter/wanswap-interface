a8cb79d56e87793b2ab27c42e66356d03ef87145
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2020-12-12 20:35:28 -0500

    Update index.tsx

--- src/pages/Earn/index.tsx
+++ src/pages/Earn/index.tsx
@@ -51,11 +51,11 @@ export default function Earn() {
           <CardSection>
             <AutoColumn gap="md">
               <RowBetween>
-                <TYPE.white fontWeight={600}>Wanswap liquidity mining</TYPE.white>
+                <TYPE.white fontWeight={600}>Bigswap liquidity mining</TYPE.white>
               </RowBetween>
               <RowBetween>
                 <TYPE.white fontSize={14}>
-                  Deposit your Liquidity Provider tokens to receive WASP, the Wanswap protocol governance token.
+                  Deposit your Liquidity Provider tokens to receive BGSP, the Bigswap protocol governance token.
                 </TYPE.white>
               </RowBetween>{' '}
               <ExternalLink
@@ -63,7 +63,7 @@ export default function Earn() {
                 href="https://medium.com/wanswap/introducing-wanswap-the-wanchain-based-cross-chain-decentralized-exchange-with-automated-market-5e5f5956c223"
                 target="_blank"
               >
-                <TYPE.white fontSize={14}>Read more about WASP</TYPE.white>
+                <TYPE.white fontSize={14}>Read more about BGSP</TYPE.white>
               </ExternalLink>
             </AutoColumn>
           </CardSection>
